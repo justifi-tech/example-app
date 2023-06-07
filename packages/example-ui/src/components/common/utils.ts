33ea53c53428ef4b3ea771ff37b26707b078f5e8
Tomás Chernov <Chernatom@gmail.com>
2023-06-06 22:02:01 -0300

    refactor entity link to be more robust

--- packages/example-ui/src/components/common/utils.ts
+++ packages/example-ui/src/components/common/utils.ts
@@ -4,4 +4,10 @@ const formatCentsToDollars = (amount: number | undefined) => {
   return `$${dollars.toFixed(2)}`;
 };
 
-export { formatCentsToDollars }
+const buildEntityUrl = (account: string, entityId: string) => 
+  `${process.env.REACT_APP_JUSTIFI_DASHBOARD_URL}/account/${account}/payments/${entityId}`
+
+export {
+  formatCentsToDollars,
+  buildEntityUrl
+}
